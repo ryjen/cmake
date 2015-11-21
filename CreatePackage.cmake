be91ee84adea948dd2193333d198e8790389b0ac
Ryan Jennings <ryan@arg3.com>
2015-11-21 15:34:02 -0800

    Add function to create a pkg-config config file

---
+++ CreatePackage.cmake
@@ -0,0 +1,19 @@
+
+if ("${CMAKE_PROJECT_NAME}" EQUAL "")
+	message(FATAL_ERROR "Must define a projectbefore using create_package")
+endif()
+
+set(THIS_FILE_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+function(create_package _description)
+
+	set(PROJECT_DESCRIPTION _description)
+
+	message(STATUS "Creating package config file ${CMAKE_PROJECT_NAME}.pc")
+
+	configure_file("${THIS_FILE_DIR}/pkg-config.pc.in" "${CMAKE_SOURCE_DIR}/${CMAKE_PROJET_NAME}.pc" @ONLY)
+
+	install(FILES "${CMAKE_SOURCE_DIR}/${CMAKE_PROJECT_NAME}.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
+
+endfunction()
+
