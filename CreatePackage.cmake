029f0525e610ed623a614aa4b269e4a52c2054aa
Ryan Jennings <ryan@arg3.com>
2015-11-21 15:48:49 -0800

    update create_package

--- CreatePackage.cmake
+++ CreatePackage.cmake
@@ -1,5 +1,5 @@
 
-if ("${CMAKE_PROJECT_NAME}" EQUAL "")
+if ("${PROJECT_NAME}" EQUAL "")
 	message(FATAL_ERROR "Must define a project before using create_package")
 endif()
 
@@ -13,11 +13,12 @@ function(create_package _description)
 
 	set(PROJECT_DESCRIPTION _description)
 
-	message(STATUS "Creating package config file ${CMAKE_PROJECT_NAME}.pc")
+	message(STATUS "Creating package config file ${PROJECT_NAME}.pc")
 
-	configure_file("${THIS_FILE_DIR}/pkg-config.pc.in" "${CMAKE_SOURCE_DIR}/${CMAKE_PROJET_NAME}.pc" @ONLY)
+	configure_file("${THIS_FILE_DIR}/pkg-config.pc.in" "${CMAKE_CURRENT_BINARY_DIR}/${PROJET_NAME}.pc" @ONLY)
 
-	install(FILES "${CMAKE_SOURCE_DIR}/${CMAKE_PROJECT_NAME}.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
+	install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
 
 endfunction()
 
+cd ..
