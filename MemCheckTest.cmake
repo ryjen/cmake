45ec579723d2d0e776f7e9e527cdc7b2f3507536
Ryan Jennings <ryan@arg3.com>
2015-11-20 20:26:38 -0800

    small updates

--- MemCheckTest.cmake
+++ MemCheckTest.cmake
@@ -2,7 +2,7 @@
 find_program(MEMCHECK_COMMAND valgrind)
 
 if (MEMCHECK_COMMAND)
-	SET(MEMCHECK_FOUND ON "Memory checking support found.")
+	SET(MEMCHECK_FOUND ON CACHE STRING "Memory checking support found.")
 endif()
 
 function(add_memcheck_test _targetname _testrunner)
@@ -10,7 +10,7 @@ function(add_memcheck_test _targetname _testrunner)
 	if (NOT MEMCHECK_COMMAND)
 		MESSAGE( "Memcheck command not found!")
 		add_test(${_targetname} ${_testrunner})
-	else(MEMCHECK_COMMAND)
+	else ()
 		add_test(${_targetname} ${MEMCHECK_COMMAND} --leak-check=full --error-exitcode=5 ${ARGV2} --quiet ${_testrunner})
 	endif()
 
