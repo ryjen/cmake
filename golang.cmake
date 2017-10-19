d30cece554bb2658b8603b8fa7fa32f380989c60
Ryan Jennings <info@ryan-jennings.net>
2017-10-18 21:25:03 -0700

    Update golang script

--- golang.cmake
+++ golang.cmake
@@ -1,8 +1,8 @@
 
 function(external_go_project)
   set(options)
-  set(singleValueOpts TARGET)
-  set(multiValueOpts ARGS)
+  set(singleValueOpts TARGET PACKAGE)
+  set(multiValueOpts)
 
   cmake_parse_arguments(GO_EXTERNAL "${options}" "${singleValueOpts}" "${multiValueOpts}" ${ARGN})
 
@@ -10,29 +10,33 @@ function(external_go_project)
     message(FATAL_ERROR "No target specified for external_go_project")
   endif()
 
-  if (NOT GO_EXTERNAL_ARGS)
-    message(FATAL_ERROR "No ARGS specified for external_go_project")
+  if (NOT GO_EXTERNAL_PACKAGE)
+    message(FATAL_ERROR "No Package specified for external_go_project")
   endif()
 
-  add_custom_target(${TARGET} env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} get ${ARGS})
+  add_custom_target(${GO_EXTERNAL_TARGET} env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} get ${GO_EXTERNAL_PACKAGE})
 endfunction()
 
 function(add_go_executable)
   set(options)
-  set(singleValueOpts NAME)
+  set(singleValueOpts TARGET NAME)
   set(multiValueOpts SOURCE_FILES)
 
   cmake_parse_arguments(GO_EXECUTABLE "${options}" "${singleValueOpts}" "${multiValueOpts}" ${ARGN})
 
+  if (NOT GO_EXECUTABLE_TARGET)
+    message(FATAL_ERROR "No target provided to add_go_executable")
+  endif()
+
   if (NOT GO_EXECUTABLE_NAME)
-    message(FATAL_ERROR "No name provided to add_go_executable")
+    set(GO_EXECUTABLE_NAME ${GO_EXECUTABLE_TARGET})
   endif()
 
   if (NOT GO_EXECUTABLE_SOURCE_FILES)
     file(GLOB GO_EXECUTABLE_SOURCE_FILES RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
   endif()
 
-  add_custom_target(${GO_EXECUTABLE_NAME}  COMMAND GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
+  add_custom_target(${GO_EXECUTABLE_TARGET}  COMMAND GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
     -o "${CMAKE_CURRENT_BINARY_DIR}/${GO_EXECUTABLE_NAME}"
     ${CMAKE_GO_FLAGS} ${GO_EXECUTABLE_SOURCE_FILES}
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
@@ -43,13 +47,17 @@ endfunction()
 
 function(add_go_library)
   set(options STATIC SHARED)
-  set(singleValueOpts NAME)
+  set(singleValueOpts TARGET NAME)
   set(multiValueOpts SOURCE_FILES)
 
   cmake_parse_arguments(GO_LIBRARY "${options}" "${singleValueOpts}" "${multiValueOpts}" ${ARGN})
 
+  if (NOT GO_LIBRARY_TARGET)
+    message(FATAL_ERROR "No target provided for go library")
+  endif()
+
   if (NOT GO_LIBRARY_NAME)
-    message(FATAL_ERROR "No name provided for go library")
+    set(GO_LIBRARY_NAME ${GO_LIBRARY_TARGET})
   endif()
 
   if (GO_LIBRARY_SHARED)
@@ -68,7 +76,7 @@ function(add_go_library)
     file(GLOB GO_LIBRARY_SOURCE_FILES RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
   endif()
 
-  add_custom_target(${GO_LIBRARY_NAME} COMMAND GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
+  add_custom_target(${GO_LIBRARY_TARGET} COMMAND GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
     -o "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}"
     ${CMAKE_GO_FLAGS} ${GO_LIBRARY_SOURCE_FILES}
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
